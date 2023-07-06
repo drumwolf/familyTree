9a8de5084bf77c574bddde36872aa2edd8d3fef0
drumwolf <drumwolf@drumwolfs-MacBook-Pro.local>
2023-07-06 09:10:58 -0700

    implemented HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { fetchUrl } from './services'
+import { FamilyTree } from './components'
+import type { PersonType } from './types'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [familyTree, setFamilyTree] = useState<PersonType[][][]>([])
+
+  useEffect(() => {
+    fetchUrl().then((data) => setFamilyTree(data))
+  }, [])
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FamilyTree data={familyTree} />
   )
 }
 
